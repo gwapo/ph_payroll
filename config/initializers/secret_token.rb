596d5b99441700bb7a9996f163ed9e94771a921c
Dennis Carique <denniscarique@gmail.com>
2011-07-09 14:23:11 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhPayroll::Application.config.secret_token = '962d1511caeb6bc6ca319287c5124c8fb3ab093cfc984fe244a59d1ebf5dc695de67e541c710b751d1dcedd13b4aab93340f97c2c37e3107012f5e921c77daeb'
